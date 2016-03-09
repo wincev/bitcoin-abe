14330aeafda631b2a6b083d42391c05bcbea03bb
Rikski <hendrik-richter@freenet.de>
2016-03-09 21:07:56 +0100

    Update properties

--- Abe/Chain/Worldleadcurrency.py
+++ Abe/Chain/Worldleadcurrency.py
@@ -15,25 +15,12 @@
 # <http://www.gnu.org/licenses/agpl.html>.
 
 from .Sha256NmcAuxPowChain import Sha256NmcAuxPowChain
-from . import SCRIPT_TYPE_UNKNOWN
-from ..deserialize import opcodes
 
 class Worldleadcurrency(Sha256NmcAuxPowChain):
-    """
-    Namecoin represents name operations in transaction output scripts.
-    """
+    
     def __init__(chain, **kwargs):
         chain.name = 'Worldleadcurrency'
         chain.code3 = 'WLC'
         chain.address_version = '\x00'
         chain.magic = '\x5b\x6c\x44\xa4'
         Sha256NmcAuxPowChain.__init__(chain, **kwargs)
-
-    _drops = (opcodes.OP_NOP, opcodes.OP_DROP, opcodes.OP_2DROP)
-
-    def parse_decoded_txout_script(chain, decoded):
-        start = 0
-        pushed = 0
-
-    datadir_conf_file_name = "worldleadcurrency.conf"
-    datadir_rpcport = 9966
